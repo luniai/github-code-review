38960fa1d9333531f9792e6af27f75c6718d1fa7
Paulo Henrique <paulo.scape@gmail.com>
2024-09-10 18:28:19 -0700

    chore: fix minify error

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import { fileURLToPath } from "url";
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Minify was causing extension error
+    minify: false,
     rollupOptions: {
       input: {
         main: fileURLToPath(new URL("./index.html", import.meta.url)),
